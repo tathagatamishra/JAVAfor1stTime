29b64fbbb14f5a2a47f74d0d601562da0be84c99
Tathagata Mishra <tathagatamishra007@gmail.com>
2026-04-09 23:29:22 +0530

    strings

--- JavaBasics/StringType.java
+++ JavaBasics/StringType.java
@@ -13,6 +13,7 @@ public class StringType {
         System.out.println(japName);
         
 
+        // comparison
         String txt1 = "Java";
         String txt2 = "C++";
         String txt3 = "java";
@@ -23,5 +24,46 @@ public class StringType {
 
         System.out.println(sameTxt);
         System.out.println(notSameTxt);
+
+
+        // Extract substring
+        String phrase = "Good game well played";
+
+        String sub = phrase.substring(10, phrase.length());
+
+        System.out.println(sub);
+
+
+        // Split string
+        String[] words = phrase.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]);
+        }
+
+
+        // Join string
+        String ggwp = String.join(" ", words);
+
+        System.out.println(ggwp);
+
+
+        // upper and lower case
+        String userName = "  CYB3RPSYCH0  ";
+
+        System.out.println(userName.toLowerCase());
+        System.out.println(userName.toUpperCase());
+
+
+        // trim
+        userName = userName.trim();
+
+        System.out.println(userName);
+
+
+        // replace
+        String glhf = ggwp.replace("game well played", "luck have fun");
+
+        System.out.println(glhf);
     }
 }
